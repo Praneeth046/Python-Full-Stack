7eddfb0c2c90f5d9b116eaa0aef2d7efd263108c
Praneeth046 <praneethv628@gmail.com>
2026-02-14 10:32:11 +0530

    higher order functions

--- python core/test.py
+++ python core/test.py
@@ -1,5 +1,32 @@
-nested_list = [[1, 2], [3, 4], [5, 6]]
+# 1 Use map() with a lambda to add 5 to every element of the following nested
+#list [[1, 2], [3, 4], [5, 6]]
 
-result = list(map(lambda sublist: list(map(lambda x: x + 5, sublist)), nested_list))
+# l = [[1, 2], [3, 4], [5, 6]]
+# k = list(map(lambda x: x + [5], l))
+# print(k)
 
-print(result)
+## 2
+
+d={"apple":100,"banana":40,"cherry":150}
+print(d.keys())
+print(d.values())
+print(d.items())
+f=dict(filter(lambda x:x[1]>50,d.items()))
+print(f)
+
+
+## 3
+
+# k=list(map(int,input().split()))
+# m=-10**6
+# for i in k:
+#     if m<i:
+#         m=i
+# from functools import reduce
+# print(reduce(lambda x,y: x if x>y else y, k))
+# print(m)
+
+
+
+
+
