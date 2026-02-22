2eef99e8ede240ebd1159281b7fff3152d913c31
Praneeth046 <praneethv628@gmail.com>
2026-02-22 20:33:45 +0530

    python

--- python programming/pp pratice.py
+++ python programming/pp pratice.py
@@ -1,11 +1,26 @@
-def is_prime(num):
-    fc=0
-    for i in range(1,num+1):
-        if num%i==0:
-            fc+=1
-    if fc==2:
-        print("prime")
-    else:
-        print("not prime")
-n=int(input())
-is_prime(n)
+##Given number is prime or not
+
+# def is_prime(num):
+#     fc=0
+#     for i in range(1,num+1):
+#         if num%i==0:
+#             fc+=1
+#     if fc==2:
+#         print("prime")
+#     else:
+#         print("not prime")
+# n=int(input())
+# is_prime(n)
+
+
+## To print alternative odd numbers in a given range
+
+a=int(input())
+b=int(input())
+c=0
+if a<b:
+    for i in range(a,b+1):
+        if i%2==1:
+            c+=1
+            if c%2==1:
+                print(i)
