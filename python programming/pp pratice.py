ae9f199a363276e0cd882e2cfe444c26aa7f3259
Praneeth046 <praneethv628@gmail.com>
2026-02-23 16:54:46 +0530

    python

--- python programming/pp pratice.py
+++ python programming/pp pratice.py
@@ -1,4 +1,6 @@
 ##Given number is prime or not
+from operator import truediv
+
 
 # def is_prime(num):
 #     fc=0
@@ -15,12 +17,40 @@
 
 ## To print alternative odd numbers in a given range
 
-a=int(input())
-b=int(input())
-c=0
-if a<b:
-    for i in range(a,b+1):
-        if i%2==1:
-            c+=1
-            if c%2==1:
-                print(i)
+# a=int(input())
+# b=int(input())
+# c=0
+# if a<b:
+#     for i in range(a,b+1):
+#         if i%2==1:
+#             c+=1
+#             if c%2==1:
+#                 print(i)
+
+
+##To print average of alternative prime numbers
+
+# def is_prime(n):
+#     fc=0
+#     for i in range(1,n+1):
+#         if n%i==0:
+#             fc+=1
+#         if fc==2:
+#             return True
+#         else:
+#             return False
+# a=int(input())
+# b=int(input())
+# c=ac=sum=0
+# for i in range(a,b+1):
+#     n=is_prime(i)
+#     if n==True:
+#         c+=1
+#         if c%2==1:
+#             sum=sum+i
+#             ac+=1
+# print(sum/ac)
+
+
+
+##
