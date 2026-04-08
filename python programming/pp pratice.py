3e8d1380d9959604b09e6349f96fa8f4766eb5cd
Praneeth046 <praneethv628@gmail.com>
2026-04-09 00:18:07 +0530

    python

--- python programming/pp pratice.py
+++ python programming/pp pratice.py
@@ -171,10 +171,23 @@ from operator import truediv
 
 ## hallow patterns square
 
+# n=int(input())
+# for i in range(1,n+1):
+#     for j in range(1,n+1):
+#         if i==1 or i==n or j==1 or j==n:
+#             print("*",end=" ")
+#         else:
+#             print(" ",end=" ")
+#     print()
+
+## hallow patterns  triangle
+
 n=int(input())
 for i in range(1,n+1):
-    for j in range(1,n+1):
-        if i==1 or i==n or j==1 or j==n:
+    for j in range(1,n-i+1):
+        print(" ",end=" ")
+    for j in range(1,i+1):
+        if j==1 or i==n or i==j:
             print("*",end=" ")
         else:
             print(" ",end=" ")
