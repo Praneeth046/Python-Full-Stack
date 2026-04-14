59e5858c4b0a92a73a38764365b16f4d181efc66
Praneeth046 <praneethv628@gmail.com>
2026-04-14 21:55:32 +0530

    python

--- python programming/pp pratice.py
+++ python programming/pp pratice.py
@@ -185,7 +185,7 @@ from operator import truediv
 n=int(input())
 for i in range(1,n+1):
     for j in range(1,n-i+1):
-        print(" ",end=" ")
+        print(" ",end="")
     for j in range(1,i+1):
         if j==1 or i==n or i==j:
             print("*",end=" ")
