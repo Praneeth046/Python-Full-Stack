eef8e499a9652589ad111d7673077c705b4f8bdc
Praneeth046 <praneethv628@gmail.com>
2026-04-17 18:51:42 +0530

    python

--- python programming/dsa.py
+++ python programming/dsa.py
@@ -9,5 +9,11 @@ l=[10,20,30,40,50,60,70,80,90]
 # l.append(200)
 # print(l)
 
-l.insert(0,500)
-print(l)
+# l.insert(0,500)
+# print(l)
+
+## merging two lists
+list1=list(map(int,input().split()))
+list2=list(map(int,input().split()))
+merge=list1 + list2
+print(merge)
