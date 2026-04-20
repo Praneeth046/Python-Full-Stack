5331a8d44754df1198b01e929ead011af0b15b00
Praneeth046 <praneethv628@gmail.com>
2026-04-20 12:47:12 +0530

    python

---
+++ python programming/list.py
@@ -0,0 +1,32 @@
+l=[10,20,30,40,50,60,70,80,90]
+# print(l)
+
+# for i in l:
+#     print(l)
+
+# print(l[-2])
+
+# l.append(200)
+# print(l)
+
+# l.insert(0,500)
+# print(l)
+
+## merging two lists
+# list1=list(map(int,input().split()))
+# list2=list(map(int,input().split()))
+# merge=list1 + list2
+# print(merge)
+
+## reversing the elements in list
+l=list(map(int,input().split()))
+s=0
+e=len(l)-1
+while s<e:
+    t=l[s]
+    l[s]=l[e]
+    l[e]=t
+    s+=1
+    e-=1
+print(l)
+
