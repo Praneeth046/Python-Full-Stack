448862255e731f600291dc66a70c6fa1ce3626c7
Praneeth046 <praneethv628@gmail.com>
2026-04-30 21:30:47 +0530

    python

--- python programming/list.py
+++ python programming/list.py
@@ -31,11 +31,41 @@ l=[10,20,30,40,50,60,70,80,90]
 # print(l)
 
 ## avg of odd numbers in list
+# l=list(map(int,input().split()))
+# sum=0
+# c=0
+# for i in l:
+#     if i%2==1:
+#         sum=sum+i
+#         c+=1
+# print(sum/c)
+
+## Rotation of list
+## one way
 l=list(map(int,input().split()))
-sum=0
-c=0
-for i in l:
-    if i%2==1:
-        sum=sum+i
-        c+=1
-print(sum/c)
+for i in range(0,len(l)):
+    t=l[0]
+    for j in range(0,len(l)-1):
+        l[j]=l[j+1]
+    l[len(l)-1]=t
+    print(l)
+
+## another way
+l=list((map(int,input().split())))
+for i in range(len(l)):
+    t=l[len(l)-1]
+    for j in range(len(l)-1,0,-1):
+        l[j]=l[j-1]
+    l[0]=t
+    print(l)
+
+
+
+
+
+
+
+
+
+
+
