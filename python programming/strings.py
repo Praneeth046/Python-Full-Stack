5ac7630017b0d3df95af4a9c9d4c984619f53fd8
Praneeth046 <praneethv628@gmail.com>
2026-05-05 00:46:29 +0530

    python

--- python programming/strings.py
+++ python programming/strings.py
@@ -1,10 +1,16 @@
 ## Aadhar validation
 
+# s=input()
+# if len(s)==12:
+#     for i in range(0,len(s)):
+#         if not (s[i]>="0" and s[i]<="9"):
+#             print("valid")
+#             break
+# else:
+#     print("invalid")
+
+
+## Find the length of string
 s=input()
-if len(s)==12:
-    for i in range(0,len(s)):
-        if (s[i]>="0" and s[i]<="9"):
-            print("valid")
-            break
-else:
-    print("invalid")
+n=len(s)
+print(n)
